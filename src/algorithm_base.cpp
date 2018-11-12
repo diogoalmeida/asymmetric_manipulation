16e6098e243c5905e3df8718f22a7e5bb5d42d1b
Diogo Almeida <diogoa@kth.se>
2018-11-12 10:54:02 -0500

    Add missing algorithm base

---
+++ src/algorithm_base.cpp
@@ -0,0 +1,56 @@
+#include <coordination_algorithms/algorithm_base.hpp>
+
+namespace coordination_algorithms
+{
+  AlgorithmBase::AlgorithmBase()
+  {
+    nh_ = ros::NodeHandle("~");
+
+    if (!init())
+    {
+      throw std::logic_error("Missing parameters for the algorithm");
+    }
+  }
+
+  bool AlgorithmBase::init()
+  {
+    if (!nh_.getParam("kinematic_chain_base_link", base_))
+    {
+      ROS_ERROR("Missing kinematic_chain_base_link parameter");
+      return false;
+    }
+
+    kdl_manager_ = std::make_shared<generic_control_toolbox::KDLManager>(base_);
+
+    if (!setArm("eef1", eef1_))
+    {
+      return false;
+    }
+
+    if (!setArm("eef2", eef2_))
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool AlgorithmBase::setArm(const std::string &arm_name, std::string &eef_name)
+  {
+    generic_control_toolbox::ArmInfo info;
+
+    if (!generic_control_toolbox::getArmInfo(arm_name, info))
+    {
+      return false;
+    }
+
+    eef_name = info.kdl_eef_frame;
+
+    if (!generic_control_toolbox::setKDLManager(info, kdl_manager_))
+    {
+      return false;
+    }
+
+    return true;
+  }
+}
